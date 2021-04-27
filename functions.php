3f3cdc1a36e46dce50dc2d3944d11c5881753873
Vladyslav Kukil <vkukil@corp.web4pro.com.ua>
2021-04-27 19:04:21 +0300

    created functions.php in feature branch

--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
+<<<<<<< HEAD
   function get_profile( $id ){
    $profile = get_user_by( ‘id’, $id );
    return  $profile;
   }
 ?>
+=======
+  function get_user_meta( $id, $key ){
+   return get_metadata('user', $user_id, $key, $single);
+  }
+?>
+>>>>>>> created functions.php in feature branch
